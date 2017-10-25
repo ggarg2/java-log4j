89d8963984e0177afec906e77b034adf46ecb0e1
Gaurav Garg <garg.gaurav.1989it@gmail.com>
2017-10-25 16:01:47 +0530

    log4j is integrated

---
+++ src/main/java/com/lara/Bootstrap.java
@@ -0,0 +1,16 @@
+package com.lara;
+
+import org.apache.log4j.Logger;
+
+public class Bootstrap {
+	
+	final static Logger LOG = Logger.getLogger(Bootstrap.class);
+
+	public static void main(String[] args) {
+		
+
+		LOG.debug("Hello World Debug");
+		
+	}
+	
+}
